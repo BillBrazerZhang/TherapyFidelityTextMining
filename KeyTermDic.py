1fed4dc4e7fc1370d09d09532cf1e4147dedfed3
Wenyu Zhang <931053259@qq.com>
2018-08-04 17:59:40 -0700

    update dictionary

---
+++ KeyTermDic.py
@@ -0,0 +1,31 @@
+# Dictionary of key-terms for CTS fidelity
+keywordCTS = defaultdict(set)
+
+keywordCTS['Agenda'] = {'agenda',['priorities','most important','focus on first'],['talk about today','work on today','focus on during the session','work on'] \
+                        ,['you like to add to the agenda','you like to add anything to the agenda','you want to add to the agenda' \
+                        ,'you want to add anything to the agenda'],'last week'}
+keywordCTS['Feedback'] = {'feedback',['previous','last time','last week','last session','past session'] \
+                          ,['think about today','things go today','think about today\'s session'],'concern','unhelpful','helpful' \
+                          ,['anything i can do better','anything we can do better'],['concerns about today\'s session','helpful about the session'] \
+                          ,'learn','skills','achieve','goals',['if i understand you correctly', 'are you saying', 'do i have it right']}
+keywordCTS['Understanding'] = {'understand','understanding','sounds like','see','make sense'}
+keywordCTS['Interpersonal Effectiveness'] = {'sorry','hard','difficult','tough' \
+                                             ,'dissappointing','stressful','stressed' \
+                                             ,'scary','frightening','upset','upsetting'\
+                                             ,'unfortunate'}
+keywordCTS['Collaboration'] = {'choice', 'want','because','will','help','get','goal'}
+keywordCTS['Guided Discovery'] = {'meaning','mean','self','how','why','evidence' \
+                                  ,'conclusion','conclude','decide','decision','decided' \
+                                  ,'know','proof','tell','more','assume','assumption' \
+                                  ,'hypothesis','disprove','facts','fact','solutions' \
+                                  ,'brainstorm','solve','alternative','other','explantion' \
+                                  ,'another','way','think','about','reason'}
+keywordCTS['Focus on Key Cognitions'] = {'thinking','tell','yourself','through','mind' \
+                                         ,'thought','think','connection','lead','connected' \
+                                         ,'connect','link','linked','make','do','did' \
+                                         ,'thoughts','feelings','felt','action','behavior' \
+                                         ,'goal'}
+keywordCTS['Choices of Intervention'] = {}
+keywordCTS['Homework'] = {'homework','review','home','practice','assignment','assign' \
+                          ,'assigned','progress','learned','improve','learn','skills' \
+                          ,'goal','better','barrier','expect','problems','succeed','success'}
