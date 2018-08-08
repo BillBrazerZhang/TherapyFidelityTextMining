d5c8080e8a93c28153a384b97dd24560a89ce1ee
Wenyu Zhang <931053259@qq.com>
2018-08-07 20:53:26 -0700

    questionmark added

--- KeyTermDic.py
+++ KeyTermDic.py
@@ -4,28 +4,37 @@ keywordCTS = defaultdict(set)
 keywordCTS['Agenda'] = {'agenda',['priorities','most important','focus on first'],['talk about today','work on today','focus on during the session','work on'] \
                         ,['you like to add to the agenda','you like to add anything to the agenda','you want to add to the agenda' \
                         ,'you want to add anything to the agenda'],'last week'}
+
 keywordCTS['Feedback'] = {'feedback',['previous','last time','last week','last session','past session'] \
                           ,['think about today','things go today','think about today\'s session'],'concern','unhelpful','helpful' \
                           ,['anything i can do better','anything we can do better'],['concerns about today\'s session','helpful about the session'] \
                           ,'learn','skills','achieve','goals',['if i understand you correctly', 'are you saying', 'do i have it right']}
-keywordCTS['Understanding'] = {'understand','understanding','sounds like','see','make sense'}
+
+keywordCTS['Understanding'] = {['understand','understanding'],'sounds like',['you are saying','you are feeling'],['you were feeling','you felt'] \
+                                ,['see','makes sense','i see'],['feel that way','feel this way']}
+
 keywordCTS['Interpersonal Effectiveness'] = {'sorry','hard','difficult','tough' \
                                              ,'dissappointing','stressful','stressed' \
                                              ,'scary','frightening','upset','upsetting'\
                                              ,'unfortunate'}
-keywordCTS['Collaboration'] = {'choice', 'want','because','will','help','get','goal'}
+
+keywordCTS['Collaboration'] = {'choice', 'you want to do','good idea','because','will','help you get your goal'}
+
 keywordCTS['Guided Discovery'] = {'meaning','mean','self','how','why','evidence' \
                                   ,'conclusion','conclude','decide','decision','decided' \
-                                  ,'know','proof','tell','more','assume','assumption' \
+                                  ,'know','proof','tell me more','assume','assumption' \
                                   ,'hypothesis','disprove','facts','fact','solutions' \
-                                  ,'brainstorm','solve','alternative','other','explantion' \
-                                  ,'another','way','think','about','reason'}
-keywordCTS['Focus on Key Cognitions'] = {'thinking','tell','yourself','through','mind' \
-                                         ,'thought','think','connection','lead','connected' \
-                                         ,'connect','link','linked','make','do','did' \
-                                         ,'thoughts','feelings','felt','action','behavior' \
-                                         ,'goal'}
+                                  ,'brainstorm','solve','alternative','other explanations' \
+                                  ,'another way','other way','to think about','to explain','reason'}
+keywordCTS['Focus on Key Cognitions'] = {'thinking','tell yourself','through your mind' \
+                                         ,'thought','think','connection','lead to','connected' \
+                                         ,'connect','link','linked','make you','you do'}
+
 keywordCTS['Choices of Intervention'] = {}
-keywordCTS['Homework'] = {'homework','review','home','practice','assignment','assign' \
+
+keywordCTS['Homework'] = {'homework','review',' at home','practice','assignment','assign' \
                           ,'assigned','progress','learned','improve','learn','skills' \
-                          ,'goal','better','barrier','expect','problems','succeed','success'}
+                          ,'goal','better','barrier','in the way','expect','problems','succeed','success'}
+
+keywordCTS['Social Skills Training'] = {'rational','help you learn this skill','help you with your goal' \
+                                        ,'demonstrate','to make your next role','play better','play even better','try to focus on'}
